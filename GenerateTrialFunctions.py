dfffe987e100082bcc1850d41ae5d85c8cc7e28d
Will Wheeler <willwheelera@gmail.com>
2015-04-13 17:51:57 -0500

    Added GenerateTrialFunctions.py to create the trial wavefunctions given the initial wavefunctions

---
+++ GenerateTrialFunctions.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numpy.linalg as LA
+
+# GenerateTrialFunctions.py
+# This contains a function that takes the starting wavefunctions and creates a trial wavefunction
+# 1. Compute a Slater determinant
+# 2. Multiply a Jastrow factor
+
