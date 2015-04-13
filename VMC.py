35f744874e87602153d800f79d4c710b8f234abd
Will Wheeler <willwheelera@gmail.com>
2015-04-12 20:44:47 -0500

    Added file VMC.py

---
+++ VMC.py
@@ -0,0 +1,19 @@
+import numpy as np
+import numpy.linalg as LA
+import math
+
+
+def MC_loop(Psi, N):
+  # Psi is the trial wavefunction. This function finds the energy of Psi using Monte Carlo
+  # N is the number of electrons
+  R = InitializePositions(N)
+
+
+
+
+def InitializePositions(N):
+  R = numpy.zeros((N,3))
+  return R
+
+
+
