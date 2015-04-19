Fixed many minor bugs. Simulation runs, plots KE trace. Incorrect results :(

GenerateTrialFunctions.py
@@ -25,12 +25,13 @@ def SlaterMatrix(e_positions,psi_array):
     return slater_matrix
 
 def SlaterDeterminant(slater_matrix):
-    WF = (1/sqrt(np.math.factorial(len(e_positions)))) * LA.det(slater_matrix)
+    Nfact = np.math.factorial(len(slater_matrix))
+    WF = (1/np.sqrt(Nfact)) * LA.det(slater_matrix)
     return WF
 
 def PsiManyBody(e_positions):
-    slater_matrix = SlaterMatrix(e_positions)
-    return Slater_Determinant(slater_matrix)
+    slater_matrix = SlaterMatrix(e_positions,psi_array)
+    return SlaterDeterminant(slater_matrix)
 
 ##########################################
 # TODO don't need this function
@@ -58,8 +59,14 @@ def KineticTerm(e_positions):
     
     deriv_mat = SlaterMatrix(e_positions,psi_laplacian) # the slater matrix of the laplacians
     N = len(e_positions) # 
-    allSlaterMats = numpy.repeat(SlaterMatrix(e_positions,psi_array),N,axis=2) # copy this matrix N times along dimension 2
+
+    allSlaterMats = np.repeat([SlaterMatrix(e_positions,psi_array)],N,axis=0) # copy this matrix N times along dimension 0
+    
     for i in range(N):
-        allSlaterMats[i,:,i] = deriv_mat[i,:] # set the "diagonal rows" of this NxNxN matrix to be the second derivatives
-    localKineticEnergy = numpy.sum(LA.det(allSlaterMats)) # add together the determinants of each derivative matrix
+        allSlaterMats[i,i,:] = deriv_mat[i,:] # set the "diagonal rows" of this NxNxN matrix to be the second derivatives
+        # First index: slice
+        # Second index: matrix row (which position)
+        # Third index: matrix column (which wavefunction)
+    
+    localKineticEnergy = np.sum(LA.det(allSlaterMats)) # add together the determinants of each derivative matrix
     return localKineticEnergy

VMC.py
@@ -37,7 +37,12 @@ def MC_loop():
     
     e_positions = GSF.InitializeElectrons()
     e_positions_new = e_positions.copy()
-    collection_of_positions = numpy.zeros((2*N,3))
+    N = len(e_positions)
+    collection_of_positions = np.zeros((2*N*steps,3))
+    
+    prob_old = GTF.PsiManyBody(e_positions)**2
+    
+    E = np.zeros(steps)
     
     for t in range(0,steps):
         for i in range(0,len(e_positions)):
@@ -47,34 +52,37 @@ def MC_loop():
             e_positions_new = UpdatePosition(e_positions,i,sigma) #generate array of new electron poisitons
             
             # I don't think we need this: -Will
-            # prob_old = GTF.PsiManyBody(e_positions_old,psi_array)**2 #get modulus^2 of old wave function
+            # prob_old = GTF.PsiManyBody(e_positions_old)**2 #get modulus^2 of old wave function
             
-            prob_new = GTF.PsiManyBody(e_positions_new,psi_array)**2 #get modulus^2 of new wave function
+            prob_new = GTF.PsiManyBody(e_positions_new)**2 #get modulus^2 of new wave function
             ratio = prob_new/prob_old #take the ratio of the squares
 
             ## A = min(1,ratio) # Acceptance crtierion - this is automatically satisfied in our probability checking
-            
+            A = ratio
+
             ran = np.random.random()
             if A > ran:
                 e_positions = e_positions_new
                 moves_accepted += 1.0
                 prob_old = prob_new
-            else:
+            #else:
                 # I don't think we need this: - Will
                 # e_positions = e_positions_old
-            collection_of_positions[t:t+1,:] = e_positions
+            collection_of_positions[2*t:2*t+2,:] = e_positions
+        E[t] = GTF.KineticTerm(e_positions) # TODO this is only kinetic
+
     print('Acceptance Rate:',(moves_accepted/(2.0*t))*100.0)
     
-    return collection_of_positions
+    return collection_of_positions, E
 
 
 #############################################################
-# Run Simulation
+# RUN SIMULATIONS
 #############################################################
 
 if __name__ == '__main__':
     
-    collection_of_positions = MC_loop()
+    collection_of_positions, E = MC_loop()
 
 #############################################################
 # PLOT POSITIONS
@@ -84,7 +92,7 @@ if __name__ == '__main__':
     y = collection_of_positions[:,1]
     z = collection_of_positions[:,2]
     
-    
+    # 3D SCATTER PLOT
     #fig = plt.figure()
     #ax = fig.add_subplot(111, projection='3d')
     #ax.scatter(x, y, z, marker=".")#, zs)
@@ -93,15 +101,24 @@ if __name__ == '__main__':
     #ax.set_zlabel('Z Label')
     #plt.show()
     
+    # 2D HISTOGRAM
     nbins = 300
     H, xedges, yedges = np.histogram2d(x,y,bins=nbins)
     Hmasked = np.ma.masked_where(H==0,H)
-    fig2 = plt.figure()
+    #fig2 = plt.figure()
+    plt.subplot(2,1,1)
     plt.pcolormesh(xedges,yedges,Hmasked)
     plt.xlabel('x')
     plt.ylabel('y')
     cbar = plt.colorbar()
     cbar.ax.set_ylabel('Counts of Psi')
+    
+    # ENERGY TRACE
+    plt.subplot(2,1,2)
+    plt.plot(E)
+    plt.xlabel('Monte Carlo steps')
+    plt.ylabel('Energy')
+
     plt.show()
     
 