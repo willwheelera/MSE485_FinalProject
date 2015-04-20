Removed r^2 scaling, it wasn't correct. Also put contents of GenerateTrialFunctions into a class.

GenerateTrialFunctions.py
@@ -3,115 +3,88 @@ import numpy.linalg as LA
 import GenerateStartingFunctions as GSF
 from numpy import random
 
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
-
-
 # GenerateTrialFunctions.py
 # This contains a function that takes the starting wavefunctions and creates a trial wavefunction
 # 1. Compute a Slater determinant
-# 2. Multiply a Jastrow factor
+# 2. Multiply a Jastrow factor TODO
 
-# Define the atomic wavefunctions
-psi_array = GSF.getH2Functions()  #generate array of objective basis states
-psi_laplacian = GSF.getH2Laplacians() # get kinetic energy terms of wavefunctions (including hbar^2/2m)
-R = GSF.ion_positions
-Z = GSF.ion_charges  
-N = len(R)
-
-def setAtomicWavefunctions(wfnArray):
-    psi_array = wfnArray
-
-def SlaterMatrix(e_positions,psi_array):
-    slater_matrix = np.zeros((len(e_positions),(len(e_positions))))
-    for j in range(0, len(psi_array)):
-        slater_matrix[j,:] = psi_array[j](e_positions)  #build slater determinant
-    return slater_matrix
-
-def SlaterDeterminant(slater_matrix):
-    Nfact = np.math.factorial(len(slater_matrix))
-    WF = (1/np.sqrt(Nfact)) * LA.det(slater_matrix)
-    return WF
-
-def PsiManyBody(e_positions):
-    slater_matrix = SlaterMatrix(e_positions,psi_array)
-    print (e_positions)
-    print(R)
-    # make a "slater matrix" of displacements of electrons from ions (e-pos index is row, ion-pos index is col)
-    # This matrix is Nx3xN, 1st dim is e-pos, 2nd dim is xyz, 3rd dim is ion-pos
-    disp_matrix = np.tile([e_positions],(1,1,N)) - np.swapaxes(np.tile([R],(1,1,N)),0,2)
-    # make the "slater matrix" of distances of electrons from ions by summing squares of disp_mat.
-    dist2_matrix = np.swapaxes(np.sum(disp_matrix*disp_matrix,1),1,2)
-    # return Psi_MB and (r-R)^2Psi_MB (MB = many-body)
-    return SlaterDeterminant(slater_matrix), SlaterDeterminant(slater_matrix*dist2_matrix)
-
-##########################################
-# TODO don't need this function
-def LaplacianSlater(e_positions,i):
-    slater_mat = SlaterMatrix(e_positions)
-    second_derivs = numpy.zeros(len(e_positions))
-    for j in range(len(psi_laplacian)):
-        second_derivs[j] = psi_laplacian[j](np.array([e_positions[i]]))
-    slater_mat[i,:] = second_derivs
-    return slater_mat
-
-# This is for calculating kinetic energy
-# TODO don't need this 
-def LaplacianPsi(e_positions,i):
-    slater_mat = LaplacianSlater(e_positions,i)
-    return SlaterDeterminant(slater_mat)
-
-###########################################
-# TODO move these functions to another file
-
-def KineticTerm(e_positions):
-    # We can compute all of the kinetic energy terms given the positions
-    # This might be hard to debug...
-    # Apparently LA.det will compute determinants of all matrices stacked along dimension 2 at once
-    # I am not sure this is any faster... but less for loops :)
+class WaveFunctionClass:
+    # Define the atomic wavefunctions
+    psi_array = GSF.getH2Functions()  #generate array of objective basis states
+    psi_laplacian = GSF.getH2Laplacians() # get kinetic energy terms of wavefunctions (including hbar^2/2m)
+    R = GSF.ion_positions
+    Z = GSF.ion_charges  
+    N = len(R)
     
-    deriv_mat = SlaterMatrix(e_positions,psi_laplacian) # the slater matrix of the laplacians
-    N = len(e_positions) # 
+    def setAtomicWavefunctions(wfnArray):
+        psi_array = wfnArray
     
-    allSlaterMats = np.repeat([SlaterMatrix(e_positions,psi_array)],N,axis=0) # copy this matrix N times along dimension 0
+    def SlaterMatrix(e_positions,psi_array):
+        slater_matrix = np.zeros((len(e_positions),(len(e_positions))))
+        for j in range(0, len(psi_array)):
+            slater_matrix[j,:] = psi_array[j](e_positions)  #build slater matrix
+        return slater_matrix
+    
+    def SlaterDeterminant(slater_matrix):
+        Nfact = np.math.factorial(len(slater_matrix))
+        WF = (1/np.sqrt(Nfact)) * LA.det(slater_matrix)
+        return WF
+    
+    def PsiManyBody(e_positions):
+        slater_matrix = SlaterMatrix(e_positions,psi_array)
+        return SlaterDeterminant(slater_matrix)
+    
+    ##########################################
+    # TODO move these functions to another file (maybe)
+    
+    def LocalEnergy(e_positions, psi_at_rvec):
+        # KINETIC TERM
+        # We can compute all of the kinetic energy terms given the positions
+        # This might be hard to debug...
+        # Apparently LA.det will compute determinants of all matrices stacked along dimension 2 at once
+        # I am not sure this is any faster... but less for loops :)
         
-    if np.version.version > '1.8':
-        for i in range(N):
-            allSlaterMats[i,i,:] = deriv_mat[i,:] # set the "diagonal rows" of this NxNxN matrix to be the second derivatives
-            # First index: slice
-            # Second index: matrix row (which position)
-            # Third index: matrix column (which wavefunction)
+        deriv_mat = SlaterMatrix(e_positions,psi_laplacian) # the slater matrix of the laplacians
+        N = len(e_positions) # 
+        
+        allSlaterMats = np.repeat([SlaterMatrix(e_positions,psi_array)],N,axis=0) # copy this matrix N times along dimension 0
+            
+        if np.version.version > '1.8':
+            for i in range(N):
+                # set the "diagonal rows" of this NxNxN matrix to be the second derivatives
+                allSlaterMats[i,i,:] = deriv_mat[i,:]
+                # First index: slice
+                # Second index: matrix row (which position)
+                # Third index: matrix column (which wavefunction)
+        
+            localKineticEnergy = np.sum(LA.det(allSlaterMats)) # add together the determinants of each derivative matrix
+        else:
+            dets = np.zeros(N)
+            for i in range(N):
+                 # set the "diagonal rows" of this NxNxN matrix to be the second derivatives
+                allSlaterMats[i,i,:] = deriv_mat[i,:]
+                dets[i] = LA.det(allSlaterMats[i,:,:])
+            localKineticEnergy = np.sum(dets) / psi_at_rvec
     
-        localKineticEnergy = np.sum(LA.det(allSlaterMats)) # add together the determinants of each derivative matrix
-    else:
-        dets = np.zeros(N)
-        for i in range(N):
-            allSlaterMats[i,i,:] = deriv_mat[i,:] # set the "diagonal rows" of this NxNxN matrix to be the second derivatives
-            dets[i] = LA.det(allSlaterMats[i,:,:])
-        localKineticEnergy = np.sum(dets)
-
-    return localKineticEnergy
-
-def PotentialTerm(e_positions):
+        # POTENTIAL TERM
+        q_e2 = GSF.q_e**2
     
-    q_e2 = GSF.q_e**2
-
-    for i in range(len(e_positions)):
-        # electron-ion terms
-        S = np.repeat([e_positions[i,:]],len(R),axis=0)
-        ion_displacements = S-R
-        ion_distances = np.sqrt(np.sum(ion_displacements*ion_displacements,axis=1))
-        V_ion = np.sum(Z/ion_distances) * q_e2
-         
-        # electron-electron terms
-        S = np.repeat([e_positions[i,:]],len(e_positions)-i-1,axis=0)
-        e_displacements = S-e_positions[i+1:,:] # only calculate distances to e- not already counted
-        e_distances = np.sqrt(np.sum(e_displacements*e_displacements,axis=1))
-        V_e = np.sum(1.0/e_distances) * q_e2
-
-    return V_ion + V_e
-
-
+        for i in range(len(e_positions)):
+            # electron-ion terms
+            S = np.repeat([e_positions[i,:]],len(R),axis=0)
+            ion_displacements = S-R
+            ion_distances = np.sqrt(np.sum(ion_displacements*ion_displacements,axis=1))
+            V_ion = np.sum(Z/ion_distances) * q_e2
+             
+            # electron-electron terms
+            S = np.repeat([e_positions[i,:]],len(e_positions)-i-1,axis=0)
+            e_displacements = S-e_positions[i+1:,:] # only calculate distances to e- not already counted
+            e_distances = np.sqrt(np.sum(e_displacements*e_displacements,axis=1))
+            V_e = np.sum(1.0/e_distances) * q_e2
+    
+        return V_ion + V_e + localKineticEnergy
+    
+    
 
 
 

VMC.py
@@ -41,7 +41,7 @@ def MC_loop():
     N = len(e_positions)
     collection_of_positions = np.zeros((2*N*steps,3))
     
-    Psi, r2Psi = GTF.PsiManyBody(e_positions)
+    Psi = GTF.PsiManyBody(e_positions)
     prob_old = Psi**2
     
     E = np.zeros(steps)
@@ -57,8 +57,8 @@ def MC_loop():
             # I don't think we need this: -Will
             # prob_old = GTF.PsiManyBody(e_positions_old)**2 #get modulus^2 of old wave function
             
-            Psi_new, r2Psi_new =  GTF.PsiManyBody(e_positions_new)
-            prob_new = r2Psi_new*Psi_new #get modulus^2 of new wave function * r^2 (slater) for spherical integration
+            Psi_new =  GTF.PsiManyBody(e_positions_new)
+            prob_new = Psi_new**2 #get modulus^2 of new wave function
             ratio = prob_new/prob_old #take the ratio of the squares
 
             ## A = min(1,ratio) # Acceptance crtierion - this is automatically satisfied in our probability checking
@@ -70,7 +70,6 @@ def MC_loop():
                 moves_accepted += 1.0
                 prob_old = prob_new
                 Psi = Psi_new
-                r2Psi = r2Psi_new
             #else:
                 # I don't think we need this: - Will
                 # e_positions = e_positions_old