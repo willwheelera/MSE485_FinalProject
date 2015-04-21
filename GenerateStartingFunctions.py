Changed second derivative to spherical Laplacian. Energy is now correct.

GenerateStartingFunctions.py
@@ -28,22 +28,28 @@ Y00 = (4.0*math.pi)**(-0.5)
 
 # Radial functions
 # Note: this function requires that position coordinates are along dimension 1 of the array (not 0)
-def R10(rvec):
+def R10(rvec, Z=1.0):
   r = np.sqrt(np.sum(rvec*rvec,1))
-  return 2*a_B**(-1.5)*np.exp(-r/a_B)
+  a = a_B/Z
+  return 2*a**(-1.5)*np.exp(-r/a)
 
-def R20(rvec):
-  r = np.sqrt(np.sum(rvec*rvec,1))
-  return (2*a_B)**(-1.5)*(2.0-(r/a_B))*np.exp(-2.0*r/a_B)
+def R20(rvec, Z=1.0):
+  a = 2*a_B/Z
+  rb = np.sqrt(np.sum(rvec*rvec,1)) / a
+  return 2*a**(-1.5)*(1.0-rb)*np.exp(-rb)
   
-def Laplacian_R10(rvec):
-  r = np.sqrt(np.sum(rvec*rvec,1))
-  return 2*a_B**(-3.5)*np.exp(-r/a_B)
+def Laplacian_R10(rvec, Z=1.0):
+  a = a_B/Z
+  rb = np.sqrt(np.sum(rvec*rvec,1)) / a
+  #return 2*a**(-3.5)*np.exp(-r/a)
+  return 2*a**(-3.5)*(1.0 - 2.0/rb) * np.exp(-rb)
 
-def Laplacian_R20(rvec):
-  r = np.sqrt(np.sum(rvec*rvec,1))
-  return 4*(2*a_B)**(-3.5)*(1.5-(r/(4.0*a_B)))*np.exp(-2.0*r/a_B)
+def Laplacian_R20(rvec, Z=1.0):
+  a = 2*a_B/Z
+  rb = np.sqrt(np.sum(rvec*rvec,1)) / a # change of vars makes formula simpler: rb = r/a = r*Z / 2*a_B
+  #return 4*(2*a_B)**(-3.5)*(1.5-(r/(4.0*a_B)))*np.exp(-2.0*r/a_B)
   #return (1.0/(2.0*math.sqrt(2.0)))*a_B**(-3.5)*(1.5-(r/(4.0*a_B)))*np.exp(-2.0*r/a_B)
+  return -2*a**(-3.5)/rb * (rb-4)*(rb-1)*np.exp(-rb)
 
 ####################################
 # 

GenerateTrialFunctions.py
@@ -40,10 +40,10 @@ def H2Molecule(ion_sep):
 
 class WaveFunctionClass:
     # Define the atomic wavefunctions
-    psi_array = GSF.getH2Functions()  #generate array of objective basis states
-    psi_laplacian = GSF.getH2Laplacians() # get kinetic energy terms of wavefunctions (including hbar^2/2m)
-    ion_positions = GSF.ion_positions
-    ion_charges = GSF.ion_charges  
+    psi_array = [] # GSF.getH2Functions()  #generate array of objective basis states
+    psi_laplacian = [] # GSF.getH2Laplacians() # get kinetic energy terms of wavefunctions (including hbar^2/2m)
+    ion_positions = [] # GSF.ion_positions
+    ion_charges = [] # GSF.ion_charges  
     N = len(ion_positions)
     
     def setAtomicWavefunctions(self, wfnArray):
@@ -101,20 +101,21 @@ class WaveFunctionClass:
     
         # POTENTIAL TERM
         q_e2k = GSF.q_e**2 * GSF.k_e
-    
-        for i in range(len(e_positions)):
+        V_ion = 0
+        V_e = 0
+        for i in range(N):
             # electron-ion terms
             S = np.repeat([e_positions[i,:]],N,axis=0)
             ion_displacements = S - self.ion_positions
             ion_distances = np.sqrt(np.sum(ion_displacements*ion_displacements,axis=1))
-            V_ion = -np.sum(self.ion_charges/ion_distances) * q_e2k
-             
+            V_ion += -np.sum(self.ion_charges/ion_distances) * q_e2k
+            
             # electron-electron terms
             S = np.repeat([e_positions[i,:]],len(e_positions)-i-1,axis=0)
             e_displacements = S - e_positions[i+1:,:] # only calculate distances to e- not already counted
             e_distances = np.sqrt(np.sum(e_displacements*e_displacements,axis=1))
-            V_e = np.sum(1.0/e_distances) * q_e2k
-    
+            V_e += np.sum(1.0/e_distances) * q_e2k
+            
         return V_ion + V_e + localKineticEnergy
 
 

VMC.py
@@ -30,7 +30,7 @@ sigma_default = 0.5
 steps_default = 4000
 
 bond_distance = 1.0
-WF = GTF.HydrogenAtom()
+WF = GTF.H2Molecule(2.0)
 
 def MC_loop(steps=1000, sig=0.5):
     
@@ -85,11 +85,11 @@ def MC_loop(steps=1000, sig=0.5):
 
 if __name__ == '__main__':
     
-    steps = 1000
+    steps_input = 1000
     
     if len(sys.argv) > 1:
         steps_input = int(sys.argv[1])
-    collection_of_positions, E = MC_loop(steps=steps_input)
+    collection_of_positions, E = MC_loop(steps_input)
  
     Eavg=np.average(E)
     print 'Avg Energy: '+str(Eavg)